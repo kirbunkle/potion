5cacf0b031a351d2a452f412e96967ef94327260
kirbunkle <ahkirbunkle@gmail.com>
2018-06-09 00:55:41 -0700

    initial commit

---
+++ PotionMaster/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PotionMaster
+{
+    class Character
+    {
+        private Sprite sprite;
+        protected float moveX;
+        protected float moveY;
+
+
+        public Character()
+        {
+            sprite = new Sprite();
+            moveX = 0;
+            moveY = 0;
+        }
+
+        public void Update()
+        {
+            sprite.Update(moveX, moveY);
+        }
+
+        public void Draw()
+        {
+            sprite.Draw();
+        }
+    }
+}
---
+++ PotionMaster/PlayerCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace PotionMaster
+{
+    class PlayerCharacter : Character
+    {
+        private float speed;
+
+        public PlayerCharacter()
+        {
+            speed = 0.25f;
+        }
+
+        public new void Update()
+        {
+            moveX = 0;
+            moveY = 0;
+            if (Game1.keyboardInput.IsKeyDown(Keys.Left))
+            {
+                moveX -= speed * Game1.dt;
+            }
+            if (Game1.keyboardInput.IsKeyDown(Keys.Right))
+            {
+                moveX += speed * Game1.dt;
+            }
+            if (Game1.keyboardInput.IsKeyDown(Keys.Up))
+            {
+                moveY -= speed * Game1.dt;
+            }
+            if (Game1.keyboardInput.IsKeyDown(Keys.Down))
+            {
+                moveY += speed * Game1.dt;
+            }
+            base.Update();
+        }
+    }
+
+
+}
